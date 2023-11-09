2009d6a4e50b576d09b70592b7e67d01e0d1c328
jingyifsyd <jingyifsy@gmail.com>
2023-11-09 22:00:16 +0800

    second commit

--- main.go
+++ main.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+	fmt.Println(Add(1, 2))
+	fmt.Println(Add(2, 1))
+}
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Sub(a, b int) int {
+	return a - b
 }
